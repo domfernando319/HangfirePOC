e23d46583cade72a09962cac39637255f97d0774
domfernando319 <domfernando319@gmail.com>
2025-03-18 18:15:14 -0400

    revert

--- HangfireService/Program.cs
+++ HangfireService/Program.cs
@@ -115,7 +115,7 @@ namespace HangfireService {
             }
 
             Console.WriteLine("Tenant Background Services Running. Press ENTER to exit.");
-            await host.WaitForShutdownAsync();
+            await host.RunAsync();
         }
     }
 }
